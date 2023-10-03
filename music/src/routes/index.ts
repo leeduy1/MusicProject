eb18291554af82c45a35835d68892cf2822a47ef
theluanhoang <hoangtheluan2016@gmail.com>
2023-10-03 14:07:23 +0700

    fix: conflict

--- music/src/routes/index.ts
+++ music/src/routes/index.ts
@@ -1,11 +1,9 @@
 import Home from '~/pages/Home'
-import App from '~/App'
 import Profile from '~/pages/Profile'
 
 const publicRoutes = [
-  { path: '/', component: Home }
-  { path: '/profile', component: Profile },
-
+  { path: '/', component: Home },
+  { path: '/profile', component: Profile }
 ]
 
 export { publicRoutes }
