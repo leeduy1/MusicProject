685b748d0088c26d3af4a435c9cff79774b8e53a
Quixun <xuanphan742@gmail.com>
2023-10-03 14:17:10 +0700

    fix:confict

--- music/src/routes/index.ts
+++ music/src/routes/index.ts
@@ -1,11 +1,10 @@
 import Home from '~/pages/Home'
-import App from '~/App'
 import Profile from '~/pages/Profile'
 
 const publicRoutes = [
-  { path: '/', component: Home }
+  { path: '/', component: Home },
   { path: '/profile', component: Profile },
-
 ]
 
 export { publicRoutes }
+
